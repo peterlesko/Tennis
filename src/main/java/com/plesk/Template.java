b9b9d52c2644e726de6cc95e1abeb2ee8e6a02cd
peterlesko <peterlesko@yahoo.com>
2020-03-16 21:08:21 +0000

    Update Template.java

--- src/main/java/com/plesk/Template.java
+++ src/main/java/com/plesk/Template.java
@@ -1,9 +1,41 @@
 package com.plesk;
 
-public class Template {
+public class Tennis {
 
-    String giveBack() {
-        return "Hello";
+    public String calculateScore(int player1, int player2) {
+
+        //if ()
+
+        if ((player1 == 0) && (player2 == 0)) {
+            return "love";
+        }
+
+        if ((player1 == 1) && (player2 == 0)) {
+            return "player1: fifteen";
+        }
+
+
+        if ((player1 == 2) && (player2 == 0)) {
+            return "player1: thirty";
+        }
+
+        if ((player1 == 3) && (player2 == 0)) {
+            return "player1: forty";
+        }
+
+        if (((player1 >= 3) && (player2 >= 3)) && ((player1 == player2))) {
+            return "deuce";
+        }
+
+        if (((player1 >= 3) && (player2 >= 3)) && ((player1 % player2 == 1))) {
+            return "player1: advantage";
+        }
+
+//        if (((player1 >= 3) && (player2 >= 3)) && ((player2 % player1 == 1))) {
+//            return "player2: advantage";
+//        }
+
+        return "error";
     }
 
 }
